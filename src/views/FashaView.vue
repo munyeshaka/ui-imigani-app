08f52a803c54a05f15f374f5d4a2e4143f90efd3
munyeshaka <aimablemunyeshaka30@gmail.com>
2023-10-19 13:12:45 +0200

    imigani ogi

---
+++ src/views/FashaView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Imigani ogi ifasha gutahura ituntu nutundi mu kirundi</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <main>
+    Documentation<DocumentationIcon />
+    Tooling<ToolingIcon />
+    Ecosystem<EcosystemIcon />
+    Community<CommunityIcon />
+    Support<SupportIcon />
+  </main>
+  <h1>umugani
+author
+categ
+umwaka
+description</h1>
+</template>
+
+
+<script setup>
+import DocumentationIcon from '@/components/icons/IconDocumentation.vue'
+import ToolingIcon from '@/components/icons/IconTooling.vue'
+import EcosystemIcon from '@/components/icons/IconEcosystem.vue'
+import CommunityIcon from '@/components/icons/IconCommunity.vue'
+import SupportIcon from '@/components/icons/IconSupport.vue'
+</script>
+
